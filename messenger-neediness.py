8ad1ee71254802c9ee68128c036e2360deaeca55
Deep Toaster <deeptoaster@gmail.com>
2021-12-28 18:50:14 -0500

    display double and delayed messaging as plots

--- messenger-neediness.py
+++ messenger-neediness.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import itertools as it
 import json
+import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 import sys
 
+DELAYED_MESSAGE_THRESHOLD_S = 600
+DOUBLE_MESSAGE_THRESHOLD_S = 600
 INBOX_DIR = Path("messages/inbox")
 
 chats = [
@@ -14,10 +17,23 @@ chats = [
 ]
 for chat in chats:
     messages = pd.concat(
-        pd.json_normalize(json.load(open(chat_path)), "messages")
+        pd.json_normalize(json.load(open(chat_path)), "messages")[
+            ["sender_name", "timestamp_ms", "content"]
+        ]
         for chat_path in it.takewhile(
             lambda chat_path: chat_path.exists(),
             (chat / f"message_{chat_index}.json" for chat_index in it.count(1)),
         )
-    )
-    print(messages)
+    )[::-1]
+    messages["timestamp"] = pd.to_datetime(messages["timestamp_ms"], unit="ms")
+    messages["delayed"] = (
+        messages["timestamp_ms"].diff() >= DELAYED_MESSAGE_THRESHOLD_S * 1000
+    ) & (messages["sender_name"] != messages["sender_name"].shift())
+    messages["double"] = (
+        messages["timestamp_ms"].diff() >= DOUBLE_MESSAGE_THRESHOLD_S * 1000
+    ) & (messages["sender_name"] == messages["sender_name"].shift())
+    messages = messages.set_index("timestamp").drop("timestamp_ms", "columns")
+    messages_aggregated = messages.groupby("sender_name").resample("D").sum().unstack(0)
+    messages_aggregated["double"].plot.area(subplots=True)
+    messages_aggregated["delayed"].plot.area(subplots=True)
+plt.show()
